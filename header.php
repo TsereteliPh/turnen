732dbcdcd845d5fa324a9529750e6068101d63a3
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-09-18 16:13:37 +0300

    header update

--- header.php
+++ header.php
@@ -13,7 +13,35 @@
 
 <header class="header">
 	<div class="container header__container">
+		<a class="header__logo" aria-label="Логотип школы"></a>
 
+		<div class="header__separator"></div>
+
+		<ul class="reset-list header__menu">
+			<li class="header__menu-item">Мы на карте</li>
+			<li class="header__menu-item">Тренеры</li>
+			<li class="header__menu-item">Цены</li>
+			<li class="header__menu-item">Наш зал</li>
+			<li class="header__menu-item">Контакты</li>
+		</ul>
+
+		<button class="header__burger" type="button" aria-label="Открыть меню">
+			<span></span>
+		</button>
+	</div>
+
+	<div class="container header__drop">
+		<ul class="reset-list header__drop-menu">
+			<li class="header__menu-item">Мы на карте</li>
+			<li class="header__menu-item">Тренеры</li>
+			<li class="header__menu-item">Цены</li>
+			<li class="header__menu-item">Наш зал</li>
+			<li class="header__menu-item">Контакты</li>
+		</ul>
+
+		<ul class="header__socials">
+
+		</ul>
 	</div>
 </header>
 
