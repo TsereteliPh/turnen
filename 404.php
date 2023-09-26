fe8684467a7fe19c98c9011625af508de3670679
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-09-26 19:20:27 +0300

    add and done .not-found

--- 404.php
+++ 404.php
@@ -2,11 +2,9 @@
 
 <section class="not-found">
 	<div class="container not-found__container">
-		<div class="not-found__text">
-			<h1 class="not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
-			<p>Такой страницы не существует.</p>
-			<p>Воспользуйтесь поиском по сайту.</p>
-		</div>
+		<h1 class="title--normal not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
+		<p class="not-found__text">К сожалению такой страницы не существует. Возможно она была удалена или в запросе был указан неверный адрес.</p>
+		<a href="<?php bloginfo('url'); ?>" class="not-found__link">Вернуться на главную</a>
 	</div>
 </section>
 
