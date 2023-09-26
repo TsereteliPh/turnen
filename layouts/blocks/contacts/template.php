ca231235e840afa38cc50ae94c10446a55023427
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-09-26 13:30:46 +0300

    done .contacts

--- layouts/blocks/contacts/template.php
+++ layouts/blocks/contacts/template.php
@@ -17,39 +17,73 @@
 			<div class="contacts__socials">
 				<?php if ( $phone ) : ?>
 					<div class="st-hypten contacts__socials-wrapper">
-						<a href="tel:<?php echo preg_replace('/[^0-9,+]/', '', $phone['number']); ?>">
-							<svg width="50" height="50"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-phone"></use></svg>
+						<a href="tel:<?php echo preg_replace('/[^0-9,+]/', '', $phone['number']); ?>" class="contacts__socials-link">
+							<svg width="40" height="40"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-phone"></use></svg>
 							<?php echo $phone['number']; ?>
 						</a>
 
-						<div class="contacts__socials-desc"><?php echo $phone['desc']; ?></div>
-					</div>
-				<?php endif; ?>
-
-				<?php if ( $telegram ) : ?>
-					<div class="st-hypten contacts__socials-wrapper">
-						<a href="https://t.me/<?php echo $telegram['link']; ?>" target="_blank">
-							<svg width="50" height="50"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-telegram"></use></svg>
-							<?php echo preg_replace('/[^0-9,+]/', '', $telegram['link']); ?>
-						</a>
-
-						<div class="contacts__socials-desc"><?php echo $telegram['desc']; ?></div>
+						<?php if ( $phone['desc'] ) : ?>
+							<div class="contacts__socials-desc"><?php echo $phone['desc']; ?></div>
+						<?php endif; ?>
 					</div>
 				<?php endif; ?>
 
 				<?php if ( $whatsapp ) : ?>
 					<div class="st-hypten contacts__socials-wrapper">
-						<a href="https://wa.me/<?php echo $whatsapp['link']; ?>" target="_blank">
-							<svg width="50" height="50"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-whatsapp"></use></svg>
+						<a href="https://wa.me/<?php echo preg_replace('/[^0-9,+]/', '', $whatsapp['link']); ?>" class="contacts__socials-link" target="_blank">
+							<svg width="40" height="40"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-whatsapp"></use></svg>
 							<?php echo $whatsapp['link']; ?>
 						</a>
 
-						<div class="contacts__socials-desc"><?php echo $whatsapp['desc']; ?></div>
+						<?php if ( $whatsapp['desc'] ) : ?>
+							<div class="contacts__socials-desc"><?php echo $whatsapp['desc']; ?></div>
+						<?php endif; ?>
+					</div>
+				<?php endif; ?>
+
+				<?php
+					if ( $telegram ) :
+					if ( $telegram['type'] == 'link' ) {
+						$tgLink = preg_replace('/@/', '', $telegram['link']);
+					} else {
+						$tgLink = preg_replace('/[^0-9,+]/', '', $telegram['number']);
+					}
+				?>
+					<div class="st-hypten contacts__socials-wrapper">
+						<a href="https://t.me/<?php echo $tgLink; ?>" class="contacts__socials-link" target="_blank">
+							<svg width="40" height="40"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-telegram"></use></svg>
+							<?php echo $telegram[$telegram['type']]; ?>
+						</a>
+
+						<?php if ( $telegram['desc'] ) : ?>
+							<div class="contacts__socials-desc"><?php echo $telegram['desc']; ?></div>
+						<?php endif; ?>
 					</div>
 				<?php endif; ?>
 			</div>
 		</div>
 
+		<div class="main-form contacts__form">
+			<h3 class="main-form__title">Приходите к нам</h3>
 
+			<?php if ( get_sub_field( 'text' ) ) : ?>
+				<div class="main-form__text"><?php the_sub_field( 'text' ); ?></div>
+			<?php endif; ?>
+
+			<form method="POST" class="main-form__form">
+				<input type="text" class="input" name="client_name" placeholder="Ваше имя" required>
+
+				<input type="tel" class="input" name="client_phone" placeholder="Ваш телефон" required>
+
+				<textarea class="input main-form__textarea" name="client_message" placeholder="Время и дата для связи и комментарий"></textarea>
+
+				<button class="btn-circle main-form__submit" type="submit">Отправить</button>
+			</form>
+
+			<div class="main-form__policy">
+				Нажимая на кнопку ниже, вы соглашаетесь
+				<a href="<?php echo get_privacy_policy_url(); ?>" target="_blank">с условиями передачи данных</a>
+			</div>
+		</div>
 	</div>
 </section>
