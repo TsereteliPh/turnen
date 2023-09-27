e6fd2fac6af6451e1d992579fbb19b432f641791
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-09-27 15:33:03 +0300

    add favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/logo-main.png" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 
