5a293432dcf96940a9c597bdffcc4b8ad6090a60
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-10-09 18:12:41 +0300

    edit .welcome

--- layouts/partials/welcome.php
+++ layouts/partials/welcome.php
@@ -1,5 +1,7 @@
 <?php $welcome = get_field( 'welcome' ); ?>
 <section class="welcome" data-text="<?php echo $welcome['bg-text']; ?>">
+	<video class="welcome__video" src="<?php echo get_template_directory_uri(); ?>/assets/videos/welcome-video.mp4" type="video/mp4" autoplay muted loop></video>
+
 	<div class="container welcome__container">
 		<div class="st-hypten welcome__desc"><?php echo $welcome['desc']; ?></div>
 
