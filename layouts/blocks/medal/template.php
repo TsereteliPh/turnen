e33f250917b1f74a9f3c14a367cc8ab38dcecfcc
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-10-17 15:11:15 +0300

    edit .medal

--- layouts/blocks/medal/template.php
+++ layouts/blocks/medal/template.php
@@ -4,7 +4,7 @@
 	$tagline = get_sub_field( 'tagline' );
 ?>
 
-<section class="medal<?php echo ( $medal == 'silver' ) ? ' medal--ribbon' : ''; ?>">
+<section class="medal<?php echo ( $medal == 'silver' ) ? ' medal--silver' : ''; ?>">
 	<div class="container medal__container<?php echo ( get_sub_field( 'indent' ) ) ? ' medal__container--indent' : ''; ?>">
 
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/medal--<?php echo $medal; ?>.png" alt="Серебрянная медаль школы turnen" width="435" height="325">
