0459518a1caa3a6913b801cca81c532bfb8d9485
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-10-24 10:25:39 +0300

    add fitness365 request

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,16 +93,18 @@ function sendForm() {
 			const clientEmail = form.querySelector("input[name=client_email]");
 			const clientMessage = form.querySelector("input[name=client_message]");
 
-			if (clientName) formData.append('client_name', clientName.value);
-			if (clientTel) formData.append('client_tel', clientTel.value);
-			if (clientEmail) formData.append('client_email', clientEmail.value);
-			if (clientMessage) formData.append('client_message', clientMessage.value);
+			const fitness365User = {};
+
+			if (clientName) fitness365User['name'] = clientName.value;
+			if (clientTel) fitness365User['phone'] = clientTel.value;
+			if (clientEmail) fitness365User['email'] = clientEmail.value;
+			if (clientMessage) fitness365User['message'] = clientMessage.value;
 
 			const encodedAuth = window.btoa('1b31cbdd-8d71-4616-85a6-92f5ab1c4f83:63e6ff22-9af0-4781-b338-308e46b14ceb');
 
 			formData.append("action", "send_mail");
 
-			console.log('Basic ' + encodedAuth);
+			console.log(fitness365User);
 
 			if (formName) {
 				formData.append("form_name", formName);
@@ -116,52 +118,35 @@ function sendForm() {
 				});
 			}
 
-			// const WP_request = fetch(adem_ajax.url, {
-			// 	method: "POST",
-			// 	body: formData,
-			// })
-			// .then((response) => response.text())
-			// .then((data) => {
-			// 	Fancybox.close(true);
-			// 	form.reset();
-			// 	setTimeout(function () {
-			// 		Fancybox.show([
-			// 			{
-			// 				src: "#success",
-			// 				type: "inline",
-			// 			},
-			// 		]);
-			// 	}, 100);
-			// })
-			// .catch((error) => {
-			// 	console.error("Error:", error);
-			// });
+			const WP_request = fetch(adem_ajax.url, {
+				method: "POST",
+				body: formData,
+			})
+			.then((response) => response.text())
+			.catch((error) => {
+				console.error("Error:", error);
+			});
 
 			const fitness365 = fetch('https://w.fitness365.ru/api/v1/lead', {
 				method: "POST",
 				headers: {
 					'Authorization': 'Basic ' + encodedAuth,
 				},
-				body: {
-					name: formData.get('client_name'),
-					email: formData.get('client_email'),
-					phone: formData.get('client_tel'),
-					message: formData.get('client_message')
-				}
+				body: JSON.stringify(fitness365User)
 			})
 			.then((response) => response.text())
-			// .then((data) => {
-			// 	Fancybox.close(true);
-			// 	form.reset();
-			// 	setTimeout(function () {
-			// 		Fancybox.show([
-			// 			{
-			// 				src: "#success",
-			// 				type: "inline",
-			// 			},
-			// 		]);
-			// 	}, 100);
-			// })
+			.then((data) => {
+				Fancybox.close(true);
+				form.reset();
+				setTimeout(function () {
+					Fancybox.show([
+						{
+							src: "#success",
+							type: "inline",
+						},
+					]);
+				}, 100);
+			})
 			.catch((error) => {
 				console.error("Error:", error);
 			});
